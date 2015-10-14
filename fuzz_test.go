0a038bb0deb8fe89f3aad148811cd0843dd9df73
guotie <guotie.9@gmail.com>
2015-10-14 09:04:38 +0800

    fuzz测试要很长时间才能测完，需要增加 -timeout=999999 来测试，确保测试不会被中途杀掉

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,6 +1,7 @@
 package rsync
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	//"path"
@@ -45,17 +46,11 @@ func testFuzz(t *testing.T) {
 
 // path: file path
 // pos: 从文件的什么位置把文件分成两部分，分别作为src和dst
-func doFuzzFile(t *testing.T, path string, pos int, blocks []int) {
-	content, ierr := ioutil.ReadFile(path)
-	if ierr != nil {
-		t.Logf("read file %s failed: %s\n", path, ierr.Error())
-		return
-	}
-
+func doFuzzBytes(t *testing.T, path string, content []byte, pos int, blocks []int, debug bool) {
 	src := content[0:pos]
 	dst := content[pos:len(content)]
 	for _, b := range blocks {
-		ret := doFuzz(path, src, dst, b, true)
+		ret := doFuzz(path, src, dst, b, debug)
 		if ret != 1 {
 			t.Fail()
 		}
@@ -63,7 +58,40 @@ func doFuzzFile(t *testing.T, path string, pos int, blocks []int) {
 
 }
 
-func TestBug(t *testing.T) {
-	//doFuzzFile(t, "./testdata/corpus/1.txt", 10781, []int{2, 4, 8, 16, 32, 64, 128, 256, 512})
-	doFuzzFile(t, "./testdata/corpus/1.txt", 10781, []int{64})
+func TestFuzzManual(t *testing.T) {
+	var blocks = []int{2, 8, 16, 64, 256, 1024, 2048, 4196}
+
+	filepath.Walk("./testdata/corpus/",
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				t.Logf("path %s is dir!\n", path)
+				return nil
+			}
+			if err != nil {
+				t.Logf("Walk file %s failed: %s\n", path, err.Error())
+				return err
+			}
+			fmt.Printf("review %s:\n", path)
+			content, ierr := ioutil.ReadFile(path)
+			if ierr != nil {
+				t.Logf("read file %s failed: %s\n", path, ierr.Error())
+				return nil
+			}
+			content = append(content, content...)
+
+			// split by random
+			for i := 0; i <= len(content); i++ {
+				src := content[0:i]
+				dst := content[i:len(content)]
+				for _, b := range blocks {
+					fmt.Printf("  block=%d len(src)=%d len(dst)=%d i=%d\n",
+						b, len(src), len(dst), i)
+					ret := doFuzz(path, src, dst, b, false)
+					if ret != 1 {
+						t.Fail()
+					}
+				}
+			}
+			return nil
+		})
 }
